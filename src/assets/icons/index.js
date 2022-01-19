e46a1ac0724d8212b093f35c19d3a478deb706ac
Aritra Kar <aritrakar2002@gmail.com>
2022-01-19 00:44:02 -0500

    Cleanup

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -1,4 +1,3 @@
-export {default as HeartIcon} from "./HeartIcon"
-export {default as CalendarIcon} from "./CalendarIcon"
-export {default as InfoIcon} from "./InfoIcon"
-export {default as LinkIcon} from "./LinkIcon"
+export { default as HeartIcon } from "./HeartIcon";
+export { default as InfoIcon } from "./InfoIcon";
+export { default as LinkIcon } from "./LinkIcon";
