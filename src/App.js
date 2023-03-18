2b945e62329a02a98d4ab5a983db8b731a82a54b
Aritra Kar <aritrakar2002@gmail.com>
2023-03-18 18:47:27 -0400

    Using HashRouter instead of BrowserRouter to fix GitHub Pages deployment.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import { PostsPage, SinglePost, NotFound } from "./components";
 
 function App() {
